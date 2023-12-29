1ac0ca730fd0a001e53630550805af734724a021
Donghyeon Kim <kdh001126@gmail.com>
2023-12-28 21:42:52 -0500

    Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/sign-in" element={<Signin />}></Route>
+      <Route path="/sign-up" element={<SignUp />}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/profile" element={<Profile/>}></Route>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
