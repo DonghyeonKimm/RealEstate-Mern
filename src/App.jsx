9c71b6080c6b8131e01bf0b4efeac7bc84979171
Donghyeon Kim <kdh001126@gmail.com>
2023-12-30 14:40:07 -0500

    Create Header

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
+import Header from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/sign-in" element={<Signin />}></Route>
-      <Route path="/sign-up" element={<SignUp />}></Route>
-      <Route path="/about" element={<About/>}></Route>
-      <Route path="/profile" element={<Profile/>}></Route>
-
-    </Routes>
+    <Header>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
+      </Routes>
+    </Header>
     </BrowserRouter>
   )
 }
