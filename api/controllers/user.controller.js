05ebea5aff6e51b29319fb84c8b39cf9b255236c
Donghyeon Kim <kdh001126@gmail.com>
2024-01-02 16:16:03 -0500

    Create Sign Up api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
 export const test = (req, res) => {
         res.json({
-            message: 'Hello World',
+            message: 'Api route is working',
         });
-}
+};
--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -18,3 +18,4 @@ app.listen(3000,() =>{
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
