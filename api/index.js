38d016f098e2c44c4e70470c331a06c8df7d3502
Donghyeon Kim <kdh001126@gmail.com>
2024-01-04 17:44:46 -0500

    create a middleware and a function to hadle errors

--- api/index.js
+++ api/index.js
@@ -6,16 +6,28 @@ import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Connected to MongoDB');
+mongoose
+    .connect("mongodb+srv://kdh001126:ocnj7MOFbBBOJ3Tg@mern-estate.0czau0t.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => {
+    console.log("Connected to MongoDB!");
 }).catch((err) => {
     console.log(err);
+    console.log('not connected')
 });
 const app = express();
 
 app.listen(3000,() =>{
     console.log('Server is running on port 3000');
 });
-
+app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use((err,req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Serval Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,5 @@ import { test } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/test',test);
-
+router.get('/test', test);
 export default router;
