05d3cf4a5dede81ce3a201783b6a90a056c1d54c
Donghyeon Kim <kdh001126@gmail.com>
2024-01-09 20:06:04 -0500

    Added firebase and continue with google button

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTUs9QecdOTd8zXTU9HIY5m-soutQc3Co",
+  authDomain: "mern-estate-f8390.firebaseapp.com",
+  projectId: "mern-estate-f8390",
+  storageBucket: "mern-estate-f8390.appspot.com",
+  messagingSenderId: "1058888414348",
+  appId: "1:1058888414348:web:851f1812b2c620490277a9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
