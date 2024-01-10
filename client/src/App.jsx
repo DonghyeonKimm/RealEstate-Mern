c3febf50910c3d4a0f0e2557d51e28feae565131
Donghyeon Kim <kdh001126@gmail.com>
2024-01-09 21:26:22 -0500

    Updateheader and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,37 @@
-import { BrowserRouter,Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import Signin from "./pages/Signin";
-import SignUp from "./pages/SignUp";
-import About from "./pages/About";
-import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+// import CreateListing from './pages/CreateListing';
+// import UpdateListing from './pages/UpdateListing';
+// import Listing from './pages/Listing';
+// import Search from './pages/Search';
+
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/sign-in" element={<Signin />}></Route>
-        <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/about" element={<About/>}></Route>
-        <Route path="/profile" element={<Profile/>}></Route>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        {/* <Route path='/search' element={<Search />} />
+        <Route path='/listing/:listingId' element={<Listing />} /> */}
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          {/* <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          /> */}
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
