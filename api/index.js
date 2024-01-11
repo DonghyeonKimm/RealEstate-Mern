6bcedbf38dfee052db22e1e9e62c693f9d28f199
Donghyeon Kim <kdh001126@gmail.com>
2024-01-10 20:33:43 -0500

    Create user update api

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import {cookieParser} from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose
@@ -20,6 +20,7 @@ app.listen(3000,() =>{
     console.log('Server is running on port 3000');
 });
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use((err,req, res, next) => {
