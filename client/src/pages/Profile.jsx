27976a997b2b79f0c3728ad27de681da1db63849
Donghyeon Kim <kdh001126@gmail.com>
2024-01-14 22:07:13 -0500

    Add sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -104,7 +104,20 @@ export default function Profile() {
     dispatch(deleteUserFailure(error.message));
   }
   }
-
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
   return (
     <div className='profile-wrapper'>
       <div className='p-3 max-w-lg mx-auto'>
@@ -157,14 +170,17 @@ export default function Profile() {
             {loading ? 'Loading...' : 'Update'}
           </button>
         </form>
-        <div className="flex justify-between mt-5">
-          <span onClick = {handleDeleteUser}className='text-red-700 cursor-pointer'>
-            Delete Account
-          </span>
-          <span className='text-red-700 cursor-pointer'>
-            Sign Out
-          </span>
-        </div>
+        <div className='flex justify-between mt-5'>
+        <span
+          onClick={handleDeleteUser}
+          className='text-red-700 cursor-pointer'
+        >
+          Delete account
+        </span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
+          Sign out
+        </span>
+      </div>
         <p className='text-red-700 mt-5'>{error ? error : ''}</p>
         <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated Successfully' : ''}</p>
       </div>
