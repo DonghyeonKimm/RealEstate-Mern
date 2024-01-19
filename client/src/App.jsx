15b60adaed1d800cec4cca4de975686afa9b6886
Donghyeon Kim <kdh001126@gmail.com>
2024-01-19 00:39:23 -0500

    Created get listing test api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
-// import UpdateListing from './pages/UpdateListing';
+import UpdateListing from './pages/UpdateListing';
 // import Listing from './pages/Listing';
 // import Search from './pages/Search';
 
@@ -26,6 +26,10 @@ export default function App() {
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
