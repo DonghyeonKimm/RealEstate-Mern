6639eaac6e7c726f277453c7caf3eefdf47c5c6b
Donghyeon Kim <kdh001126@gmail.com>
2024-01-26 21:19:00 -0500

    Added contact lanlord functionality to the listing page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+//send email to the owner of the listing
+//created landlord button contact
 export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
   const [message, setMessage] = useState('');
