6157d970c746262e57010bf6494eb0dd5b60242c
Donghyeon Kim <kdh001126@gmail.com>
2024-01-26 21:05:09 -0500

    Added contact landlord api route

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
-
+import { useEffect, useState } from 'react'
+//get the landlord information
 export default function Contact({listing}) {
+    const [landlord, setLandlord] = useState(null)
+    useEffect(() => {
+        const fetchLandlord = async () => {
+            try {
+                const res = await fetch(`/api/${listing.userRef}`)
+                const data = await res.json()
+                setLandlord(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchLandlord()
+
+    },[listing.userRef])
   return (
-    <div>Contact</div>
+    {landlord && (
+
+        
+    )
+    
+    }
   )
 }
