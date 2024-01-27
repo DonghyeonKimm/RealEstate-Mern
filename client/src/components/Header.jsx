a6e52ed263568279bf1cbe2aa83ad74a799a122c
Donghyeon Kim <kdh001126@gmail.com>
2024-01-27 18:26:34 -0500

    Added use navigate to header so we can keep prev queires

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,17 @@
 import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 export default function Header() {
     const {currentUser} = useSelector(state => state.user)
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('search', searchTerm);
+        const Searchquery = urlParams.toString();
+        navigate(`/search?${Searchquery}`);
+    }
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -16,8 +25,14 @@ export default function Header() {
                 </span>
             </h1>
             </Link>
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64'></input>
+            <form onSubmit= {handleSubmit}className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button>
+                    <FaSearch className='text-slate-600'/>
+                </button>
             </form>
             <ul className='flex gap-4'>
                 <Link to='/'>
