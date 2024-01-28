921fd77ea890ea37b4693385e657411d7b3df777
Donghyeon Kim <kdh001126@gmail.com>
2024-01-27 19:06:21 -0500

    Added search page checkbox and search term box

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
-// import Search from './pages/Search';
+import Search from './pages/Search';
 
 export default function App() {
   return (
@@ -20,7 +20,7 @@ export default function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
-        {/* <Route path='/search' element={<Search />} /> */}
+        <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
 
         <Route element={<PrivateRoute />}>
---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// import ListingItem from '../components/ListingItem';
+
+export default function Search() {
+    const navigate = useNavigate();
+    const [sidebardata, setSidebardata] = useState({
+      searchTerm: '',
+      type: 'all',
+      parking: false,
+      furnished: false,
+      offer: false,
+      sort: 'created_at',
+      order: 'desc',
+    });
+  
+    const [loading, setLoading] = useState(false);
+    const [listings, setListings] = useState([]);
+    const [showMore, setShowMore] = useState(false);
+      
+  return (
+    <div className='flex flex-col md:flex-row'>
+        <div className='p-7 border-b-2 md:border-r-2
+        md:min-h-screen'>
+        <for className = 'flex flex-col gap-8'> 
+            <div className='flex items-center gap-2'>
+                <label className='whitespace-nowrap'> Search Term:</label>
+                <input type = "text"
+                id = 'searchTerm'
+                placeholder='Search...'
+                className='border rounded-lg p-3 w-full'
+                />
+            </div>
+            <div className='flex gap-2 flex-wrap items-center'>
+                <label>Type:</label>
+                <div className='flex gap-2'>
+                    <input type="checkbox" id = "all"
+                    className='w-5'/>
+                    <span>Rent & Sale</span>
+                </div>
+            </div>
+        </for>
+        </div>
+
+        <div className=''>
+        <form> </form>
+        </div>
+    </div>
+  )
+}
