890b72032346879b113cbf24d01b309ee18dd53d
Donghyeon Kim <kdh001126@gmail.com>
2024-01-29 17:31:10 -0500

    Added handle change for Search

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -78,17 +78,21 @@ export default function Search() {
                 </div>
                 <div className='flex gap-2'>
                     <input type="checkbox" id = "rent"
-                    className='w-5'/>
+                    onChange={handleChange}
+                    checked = {sidebardata.type === 'rent'}/>
                     <span>Rent</span>
                 </div>
                 <div className='flex gap-2'>
                     <input type="checkbox" id = "sale"
-                    className='w-5'/>
+                    className='w-5'
+                    onChange={handleChange}
+                    checked = {sidebardata.type === 'sale'}/>
                     <span>Sale</span>
                 </div>
                 <div className='flex gap-2'>
                     <input type="checkbox" id = "offer"
-                    className='w-5'/>
+                    className='w-5'                    onChange={handleChange}
+                    checked = {sidebardata.offer}/>
                     <span>Offer</span>
                 </div>
             </div>
@@ -96,22 +100,30 @@ export default function Search() {
                 <label className='font-semibold'>Amenities:</label>
                 <div className='flex gap-2'>
                     <input type="checkbox" id = "parking"
-                    className='w-5'/>
+                    className='w-5'onChange={handleChange}
+                    checked = {sidebardata.parking}/>
                     <span>Parking</span>
                 </div>
                 <div className='flex gap-2'>
                     <input type="checkbox" id = "furnished"
-                    className='w-5'/>
+                    className='w-5'
+                    onChange={handleChange}
+                    checked = {sidebardata.furnished}/>
                     <span>Furnished</span>
                 </div>
             </div>
             <div className = "flex items-center gap-2">
                 <label className='font-semibold'>Sort:</label>
-                <select id = "sort_order"
+                <select 
+                onChange = {handleChange}
+                defaultValue={'created_at_desc'}
+                
+                id = "sort_order"
                 className='border rounded-lg p-3'>
-                    <option>Price high to low</option>
-                    <option>Latest</option>
-                    <option>Oldest</option>
+                    <option value = 'regularPrice_desc'>Price high to low</option>
+                    <option value = 'regularPrice_asc'>Price low to high</option>
+                    <option value = 'createdAt_desc'>Latest</option>
+                    <option value = 'createdAt_asc'>Oldest</option>
                 </select>
             </div>
             <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Search</button>
