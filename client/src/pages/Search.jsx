2dbf9fb7bc0a070c8857802df2aabb75765731e8
Donghyeon Kim <kdh001126@gmail.com>
2024-01-28 21:10:32 -0500

    Added slicer for sort

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -67,11 +67,21 @@ export default function Search() {
                     <span>Furnished</span>
                 </div>
             </div>
+            <div className = "flex items-center gap-2">
+                <label>Sort:</label>
+                <select id = "sort_order"
+                className='border rounded-lg p-3'>
+                    <option>Price high to low</option>
+                    <option>Latest</option>
+                    <option>Oldest</option>
+
+                </select>
+            </div>
         </for>
         </div>
 
         <div className=''>
-        <form> </form>
+            <h1>Listing Results:</h1>
         </div>
     </div>
   )
