a7cb428a941dd256a291df232a677e1ac901639c
Donghyeon Kim <kdh001126@gmail.com>
2024-02-02 17:11:30 -0500

    Search

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ListingItem from '../components/Listingitem';
 // import ListingItem from '../components/ListingItem';
 
 export default function Search() {
@@ -192,8 +193,17 @@ export default function Search() {
         </form>
         </div>
 
-        <div className=''>
+        <div className='flex-1'>
             <h1 className='text-3xl font-semibold border-b p-3 text-slate-700'>Listing Results:</h1>
+            <div className ='p-7 flex flex-wrap gap-4'>
+    {!loading && listings.length === 0 && (
+      <p className='text-xl textslate-700 text-center w-full'>No listing found!</p>
+    )}
+    {//get each listing and map it to listing item
+      !loading && listings && listings.map((listing) => <ListingItem key = {listing._id} listing = {listing}/>)
+      
+    }
+            </div>
         </div>
     </div>
   )
